200c4ba96ce54c08125ee151ad2d884c9be0f165
wadpas <wadpas@gmail.com>
2023-11-09 18:04:52 +0200

    used StreamProvider

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class HomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final date = ref.watch(currentDate);
     final currentWeather = ref.watch(weatherProvider);
+    final names = ref.watch(namesProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -42,9 +43,29 @@ class HomePage extends ConsumerWidget {
       body: Center(
         child: Column(
           children: [
+            names.when(
+              data: (names) {
+                return Expanded(
+                  child: ListView.builder(
+                    itemCount: names.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(
+                          names.elementAt(index),
+                        ),
+                      );
+                    },
+                  ),
+                );
+              },
+              error: (error, _) => const Text("The End"),
+              loading: () => const Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
             currentWeather.when(
               data: (data) => Text(data),
-              error: (_, __) => const Text("Error 😒"),
+              error: (error, _) => const Text("Error 😒"),
               loading: () => const Center(
                 child: CircularProgressIndicator(),
               ),
@@ -88,6 +109,31 @@ class HomePage extends ConsumerWidget {
     );
   }
 }
+
+const names = [
+  'Mary',
+  'Marie',
+  'Mariam',
+  'Marina',
+  'Irene',
+];
+
+final tickerProvider = StreamProvider(
+  (ref) => Stream.periodic(
+    const Duration(seconds: 1),
+    (i) => i + 1,
+  ),
+);
+
+final namesProvider = StreamProvider(
+  (ref) {
+    return ref.watch(tickerProvider.stream).map(
+      (count) {
+        return names.getRange(0, count);
+      },
+    );
+  },
+);
 
 enum City { stockholm, paris, tokyo }
 
