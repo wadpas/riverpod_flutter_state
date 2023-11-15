d4a789a4467a98173da5f45c868a6f483f4abd30
wadpas <wadpas@gmail.com>
2023-11-15 16:17:57 +0200

    implemented AlertDialog

--- lib/main.dart
+++ lib/main.dart
@@ -39,9 +39,106 @@ class HomePage extends ConsumerWidget {
       appBar: AppBar(
         title: const Text('Riverpod'),
       ),
-      body: Center(),
+      body: Consumer(
+        builder: (context, ref, child) {
+          final dataModel = ref.watch(peopleProvider);
+          return ListView.builder(
+            itemCount: dataModel.count,
+            itemBuilder: (context, index) {
+              final person = dataModel.people[index];
+              return ListTile(
+                title: GestureDetector(
+                  child: Text(person.displayName),
+                  onTap: () async {
+                    final updatedPerson =
+                        await createOrUpdatePersonDialog(context, person);
+                    if (updatedPerson != null) {
+                      dataModel.update(updatedPerson);
+                    }
+                  },
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () async {
+          final person = await createOrUpdatePersonDialog(context);
+          if (person != null) {
+            final dataModel = ref.read(peopleProvider);
+            dataModel.add(person);
+          }
+        },
+      ),
     );
   }
+}
+
+final nameController = TextEditingController();
+final ageController = TextEditingController();
+
+Future<Person?> createOrUpdatePersonDialog(
+  BuildContext context, [
+  Person? existingPerson,
+]) {
+  String? name = existingPerson?.name;
+  int? age = existingPerson?.age;
+
+  nameController.text = name ?? '';
+  ageController.text = age?.toString() ?? '';
+
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Create a person'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                labelText: 'Enter name here...',
+              ),
+              onChanged: (value) => name = value,
+            ),
+            TextField(
+              controller: ageController,
+              decoration: const InputDecoration(
+                labelText: 'Enter age here...',
+              ),
+              onChanged: (value) => age = int.tryParse(value),
+            )
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              if (name != null && age != null) {
+                if (existingPerson != null) {
+                  final newPerson = existingPerson.updated(name, age);
+                  Navigator.of(context).pop(newPerson);
+                } else {
+                  Navigator.of(context).pop(
+                    Person(name: name!, age: age!),
+                  );
+                }
+              } else {
+                () => Navigator.of(context).pop();
+              }
+            },
+            child: const Text('Save'),
+          )
+        ],
+      );
+    },
+  );
 }
 
 @immutable
